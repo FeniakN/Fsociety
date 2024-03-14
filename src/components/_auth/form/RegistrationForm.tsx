95d8e1e0fd66f172649221df9596a690c2a08f1a
Anastasia <anastasiia.v.feniak@ukd.edu.ua>
2024-03-14 10:38:24 +0200

    add

--- src/components/_auth/form/RegistrationForm.tsx
+++ src/components/_auth/form/RegistrationForm.tsx
@@ -1,59 +1,128 @@
 import { useState } from "react";
-import style from './styles/LofinForm.module.scss'
+import style from "./styles/LofinForm.module.scss";
 import { Link } from "react-router-dom";
-import FastSingIn from "../FastSingIn/FastSingIn";
+import FastSignIn from "../FastSingIn/FastSingIn";
+import axios from "axios";
 
 const RegistrationForm = () => {
+  const [registrationData, setRegistrationData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    nickname: "",
+    birthdate: "",
+  });
 
-    const [registrationData, setRegistrationData] = useState({
-        email: '',
-        password: '',
-        confirmPassword: '',
-        nickname: '',
-        birthdate: ''
-    });
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setRegistrationData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRegistrationData({
-            ...registrationData,
-            [event.target.name]: event.target.value
-        });
-    };
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        // Тут логіка відправки даних на сервер
-        // Наприклад, fetch або axios для відправки запиту
-    };
+    // Перевірка
+    if (registrationData.password !== registrationData.confirmPassword) {
+      console.error("Ти шо пароль не той ввів?");
+      return;
+    }
 
-    return (
-        <>
-            <h2 className={style.login__title}>Become a Sailor!</h2>
+    try {
+      const response = await axios.post(
+        `https://socialnetword-fsociety.onrender.com/api/sipnup`,
+        registrationData
+      );
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">
-                    <input type="email" placeholder="Your email" id="email" name="email" required onChange={handleInputChange} />
-                </label>
-                <label htmlFor="nickname">
-                    <input type="text" placeholder="Your nickname" id="nickname" name="nickname" required onChange={handleInputChange} />
-                </label>
-                <label htmlFor="password">
-                    <input type="password" placeholder="Your password" id="password" name="password" required onChange={handleInputChange} />
-                </label>
-                <label htmlFor="confirmPassword">
-                    <input type="password" placeholder="Confirm your password" id="confirmPassword" name="confirmPassword" required onChange={handleInputChange} />
-                </label>
-                <input type="submit" value="Create an account" />
-            </form>
+      if (!response) {
+        throw new Error("Failed to register");
+      }
+      console.log("Registration successful:", response);
+      setRegistrationData({
+        email: "",
+        password: "",
+        confirmPassword: "",
+        nickname: "",
+        birthdate: "",
+      });
+    } catch (error) {
+      console.error("Error submitting: ", error);
+    }
+  };
 
-            <FastSingIn/>
-
-            <div className={style.recomendation}>
-                <p>Already have an account ? <Link className={style.recomendation__link} to="/sing-in"><strong>Sign In</strong></Link></p>
-            </div>
-
-        </>
-    );
-}
+  return (
+    <>
+      <h2 className={style.login__title}>Become a Sailor!</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          <input
+            type="email"
+            placeholder="Your email"
+            id="email"
+            name="email"
+            value={registrationData.email}
+            required
+            onChange={handleInputChange}
+          />
+        </label>
+        <label htmlFor="nickname">
+          <input
+            type="text"
+            placeholder="Your nickname"
+            id="nickname"
+            name="nickname"
+            value={registrationData.nickname}
+            required
+            onChange={handleInputChange}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            placeholder="Your password"
+            id="password"
+            name="password"
+            value={registrationData.password}
+            required
+            onChange={handleInputChange}
+          />
+        </label>
+        <label htmlFor="confirmPassword">
+          <input
+            type="password"
+            placeholder="Confirm your password"
+            id="confirmPassword"
+            name="confirmPassword"
+            required
+            onChange={handleInputChange}
+          />
+        </label>
+        <label htmlFor="birthdate">
+          <input
+            type="date"
+            id="birthdate"
+            name="birthdate"
+            value={registrationData.birthdate}
+            placeholder="Your birthdate"
+            required
+            onChange={handleInputChange}
+          />
+        </label>
+        <input type="submit" value="Create an account" />
+      </form>
+      <FastSignIn />
+      <div className={style.recommendation}>
+        <p>
+          Already have an account?
+          <Link className={style.recommendation__link} to="/sign-in">
+            <strong>Sign In</strong>
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default RegistrationForm;
